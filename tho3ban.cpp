220441db315247d58eb7c9d2141d89f0ee483bc5
mina-emad-2099 <mina.emad.2099@gmail.com>
2026-05-09 20:29:21 +0300

    added button event fo Seif's mid gui

--- tho3ban.cpp
+++ tho3ban.cpp
@@ -1,5 +1,6 @@
 #include "tho3ban.h"
 #include "ui_tho3ban.h"
+#include "game_window.h"
 
 tho3ban::tho3ban(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,21 @@ tho3ban::~tho3ban()
 {
     delete ui;
 }
+
+void tho3ban::on_pushButton_clicked()
+{
+    // 1. Create a new instance of your game window using 'new'
+    // (Note: Check your game_window.h file. Qt usually capitalizes the
+    // first letter of the class, so it might be Game_window or game_window)
+    game_window *myGame = new game_window();
+
+    // 2. Tell Qt to clean up the memory when this new window is eventually closed
+    myGame->setAttribute(Qt::WA_DeleteOnClose);
+
+    // 3. Show the new window
+    myGame->show();
+
+    // 4. Hide the main menu
+    this->hide();
+}
+
